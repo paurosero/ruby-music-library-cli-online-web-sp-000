cb7fd8ae5d9145783eae1512afb29ae2ed37580f
Paul Rosero <paul_rosero@yahoo.com>
2019-06-11 01:19:16 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,16 +7,34 @@ class MusicLibraryController
   end
   
   def call
-   puts "Welcome to your music library!"
-   puts "To list all of your songs, enter 'list songs'."
-   puts "To list all of the artists in your library, enter 'list artists'."
-   puts "To list all of the genres in your library, enter 'list genres'."
-   puts "To list all of the songs by a particular artist, enter 'list artist'."
-   puts "To list all of the songs of a particular genre, enter 'list genre'."
-   puts "To play a song, enter 'play song'."
-   puts "To quit, type 'exit'."
-   puts "What would you like to do?"
-   4.times { gets.strip}
+   input = ""
+   while input != "exit"
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    input = gets.strip
+
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
+   end
   end
   
   def list_songs
@@ -57,14 +75,11 @@ class MusicLibraryController
   
  def play_song
    puts "Which song number would you like to play?"
-   input = gets.strip.to_i
-   song_array = Song.all.sort {|a, b| a.name <=> b.name}
-   if (1..Song.all.length).include?(input)
-     song = song_array[input+2]
-     puts "Playing #{song.name} by #{song.artist.name}"
+   if (1..Song.all.length).include?(gets.strip.to_i)
+     song_array = Song.all.sort {|a, b| a.name <=> b.name}[gets.strip.to_i - 1]
    end
- 
- 
+   puts "Playing #{song_array.name} by #{song_array.artist.name}" if song_array
+ end
     
    
     
